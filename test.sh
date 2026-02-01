f6a2669e82998411658180369c67de4d94ce7b63
Gianluca Filippini <gianluca@autosburla.com>
2026-02-01 17:09:58 +0100

    adding test script

---
+++ test.sh
@@ -0,0 +1,8 @@
+#./runme.py --data_dir /media/gfilippi/bigdata_01/LOCATA_DATA/dev/ --results_dir ../blah --is_dev 1 --arrays dummy --tasks 1
+#./runme.py --data_dir ../data/dev --results_dir ../blah --is_dev 1 --arrays benchmark2 --tasks 1,2,3
+#./runme.py --data_dir /media/gfilippi/bigdata_01/LOCATA_DATA/eval/  --results_dir ../blah --is_dev 0 --arrays dummy
+
+./runme.py --data_dir /media/gfilippi/bigdata_01/LOCATA_DATA/dev/ --results_dir ../blah_matlab --is_dev 1 --arrays dummy
+#./runme.py --data_dir /media/gfilippi/bigdata_01/LOCATA_DATA/eval/  --results_dir ../blah_matlab --is_dev 0 --arrays dummy
+#./runme.py --data_dir /media/gfilippi/bigdata_01/LOCATA_DATA/eval/  --results_dir ../blah_matlab --is_dev 1 --arrays dummy
+#./runme.py --data_dir ../data/dev --results_dir ../blah --is_dev 1 --arrays benchmark2 --tasks 1,2,3
